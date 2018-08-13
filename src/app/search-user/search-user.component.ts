a6899dde222656f600f45f470cd07a7c7bdcbbf8
vivek1996 <mvivek3112@gmail.com>
2018-08-13 16:32:21 +0530

    search view testing for data

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -10,17 +10,24 @@ import { ApiService } from '../api.service';
 export class SearchUserComponent implements OnInit {
   currentUser;
   userDetails;
-  constructor(private route: ActivatedRoute, private _http: ApiService) { }
+  userRepos;
+  userFollowers;
+  userFollowing;
+  constructor(private route: ActivatedRoute, private _http: ApiService) {}
 
   ngOnInit() {
-      // Get the parameter from the URL
-      this.route.params.subscribe(routeParams => {
-        this.currentUser = routeParams.user;
-      });
+    // Get the parameter from the URL
+    this.route.params.subscribe(routeParams => {
+      this.currentUser = routeParams.user;
+      this.getUser();
+      this.getRepo();
+      this.getFollowers();
+      this.getFollowing();
+    });
   }
   // getUser data
-  getUser(name: string) {
-    this._http.getUser(name).subscribe(
+  getUser() {
+    this._http.getUser(this.currentUser).subscribe(
       data => {
         this.userDetails = data;
       },
@@ -29,5 +36,41 @@ export class SearchUserComponent implements OnInit {
       }
     );
   }
-
+  // Get repos
+  getRepo() {
+    this._http.getRepo(this.currentUser).subscribe(
+      data => {
+        let repoDemo;
+        repoDemo = data;
+        this.userRepos = repoDemo.slice(0, 10);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  // Get followers
+  getFollowers() {
+    this._http.getFollowers(this.currentUser).subscribe(
+      data => {
+        this.userFollowers = data;
+        // console.log(this.followers);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+  // Get following
+  getFollowing() {
+    this._http.getFollowing(this.currentUser).subscribe(
+      data => {
+        this.userFollowing = data;
+        // console.log(this.following);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
