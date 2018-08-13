933bd7fcd1003f1d07db56afddd31f65bd4ba03b
vivek1996 <mvivek3112@gmail.com>
2018-08-13 13:27:24 +0530

    Added search function

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import { FormControl } from '@angular/forms';
 import { CompleterService, CompleterData, RemoteData } from 'ng2-completer';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-box',
@@ -12,7 +13,10 @@ export class SearchBoxComponent implements OnInit {
 
   dataService: RemoteData;
 
-  constructor(private completerService: CompleterService) {
+  constructor(
+    private completerService: CompleterService,
+    private router: Router
+  ) {
     this.dataService = completerService.remote(null, 'login', 'login');
     this.dataService.urlFormater((term: any) => {
       return `https://api.github.com/search/users?q=${term}&per_page=5`;
@@ -22,4 +26,12 @@ export class SearchBoxComponent implements OnInit {
   }
 
   ngOnInit() {}
+  search() {
+    // console.log(this.searchStr);
+    if (this.searchStr === '' || this.searchStr.length < 2) {
+      alert('Enter More than 3 characters');
+    } else {
+      this.router.navigate(['/search', this.searchStr]);
+    }
+  }
 }
